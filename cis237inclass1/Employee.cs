f7823bce336dfe077b933cd1289f7205e0ddd63d
David Barnes <dbarnes@kvcc.edu>
2017-09-12 17:36:48 -0400

    Add Employee class. Add UserInterface class. Update Program to use both new classes.

---
+++ cis237inclass1/Employee.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass1
+{
+    class Employee
+    {
+        //******************************
+        //Variables / Backing Fields
+        //******************************
+        private string _firstName;
+        private string _lastName;
+        private decimal _weeklySalary;
+
+        //*****************************
+        //Properties
+        //*****************************
+        public string FirstName
+        {
+            get { return _firstName; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+        }
+
+        public decimal WeeklySalary
+        {
+            get { return _weeklySalary; }
+        }
+
+        //*******************************
+        //Public Methods
+        //*******************************
+        public override string ToString()
+        {
+            //Call the method below to get the first and last name
+            //and then append it to the weekly salary.
+            //Following DRY (Don't repeat yourself)
+            return this.FirstAndLastName() + " " + _weeklySalary;
+        }
+
+        public string FirstAndLastName()
+        {
+            return _firstName + " " + _lastName;
+        }
+        //*******************************
+        //Constructors
+        //*******************************
+        public Employee(string FirstName, string LastName, decimal WeeklySalary)
+        {
+            this._firstName = FirstName;
+            this._lastName = LastName;
+            this._weeklySalary = WeeklySalary;
+        }
+
+    }
+}
--- cis237inclass1/Program.cs
+++ cis237inclass1/Program.cs
@@ -10,6 +10,56 @@ namespace cis237inclass1
     {
         static void Main(string[] args)
         {
+            //Let's make a new UI class that we can use below
+            UserInterface ui = new UserInterface();
+
+            //Here is a new Employee being created just to make sure
+            //that our Employee class works
+            Employee myEmployee = new Employee("David", "Barnes", 835.00m);
+
+            Console.WriteLine(myEmployee);
+            Console.WriteLine(myEmployee.FirstAndLastName());
+
+            //Let's make an array to hold a bunch of instances of the Employee class
+            //We have the new keyword. That means memory allocation.
+            //The array is going to be in the Heap.
+            Employee[] employees = new Employee[10];
+
+            employees[0] = new Employee("James", "Kirk", 1701.00m);
+            employees[1] = new Employee("Jean-Luc", "Picard", 1701.00m);
+            employees[2] = new Employee("Benjamin", "Sisko", 587.00m);
+            employees[3] = new Employee("Kathryn", "Janeway", 287.00m);
+            employees[4] = new Employee("Johnathan", "Archer", 127.00m);
+
+
+            //Get some input from the user
+            int choice = ui.GetUserInput();
+
+            //While the user has chosen something other than 2
+            //(Which for us is only 1)
+            while (choice != 2)
+            {
+
+                //Create a string that can be concated to
+                string outputString = "";
+
+                //Print out the employees in the array
+                foreach (Employee employee in employees)
+                {
+                    if (employee != null)
+                    {
+                        //Concat the employee to the outputString
+                        outputString += employee.ToString() +
+                            Environment.NewLine;
+                    }
+                }
+                //Output the outputString to the console
+                ui.Output(outputString);
+
+                //RE-prompt the user for some new input
+                choice = ui.GetUserInput();
+            }
+
         }
     }
 }
